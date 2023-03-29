7b4a268e8cfa31b0c795db09b426f6831b37a15c
BCotterman23 <817363@mystma.org>
2023-03-29 14:35:25 -0500

    1

--- main.py
+++ main.py
@@ -1,1 +1,10 @@
 #WRITE YOUR CODE IN THIS FILE
+def hasL(w):
+    num = 0
+    for i in range(0, len(w)):
+        if w[i] == "l":
+            num = num + 1
+    if num > 0:
+        return True
+    else:
+        return False
